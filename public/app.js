0e3cbcf5790daa6353ff3f9e5a0e1e379681b5e3
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-09-23 11:25:08 -0400

    finished activity

--- public/app.js
+++ public/app.js
@@ -1,32 +1,67 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
 
+// Create map:
+const myMap = L.map("map", {
+  center: [48.868672, 2.34213],
+  zoom: 12,
+});
 
-
-// Create map
-
-
-
-
-// Add openstreetmap tiles
-
-
+// Add OpenStreetMap tiles:
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(myMap);
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
-
-// Create and add a geolocation marker
-
-
-
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453]);
+marker.addTo(myMap).bindPopup("<p1><b>The Hoxton, Paris</b></p1>").openPopup();
 
 // Draw the 2nd arrondissement
+var polygon = L.polygon([
+  // Draw the 2nd Arrondissement
+  [48.863368120198004, 2.3509079846928516],
+  [48.86933262048345, 2.3542531602919805],
+  [48.87199261164275, 2.3400569901592183],
+  [48.86993336274516, 2.3280142476578813],
+  [48.86834104280146, 2.330308418109664],
+]).addTo(myMap);
 
+//load the icon from assets folder
+var redPin = L.icon({
+  iconUrl: "assets/red-pin.png",
+  // shadowUrl: 'leaf-shadow.png',
 
-
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 // Metro station markers
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453], {
+  icon: redPin,
+}).bindPopup("RÃ©aumur-SÃ©bastopol");
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup(
+  "Strasbourg-Saint-Denis"
+);
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup(
+  "Sentier"
+);
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup(
+  "Bourse"
+);
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup(
+  "Quatre Septembre"
+);
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup(
+  "Grands Boulevards"
+);
 
-
-
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap);
 
 // Create red pin marker
